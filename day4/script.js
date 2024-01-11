05d86d85643ebded2d886065d6b77e3869458e46
Umakantk8090 <uk8418805147@gmail.com>
2024-01-11 09:55:20 +0530

    hfhsc

--- day4/script.js
+++ day4/script.js
@@ -1,10 +1,104 @@
 //map function
-
-let arr=[4,8,6,7,2];
+//1st method of map
+/*let arr=[4,8,6,7,2];
 
 function binary(x) {
     return x.toString(2);
     
 }
 const output=arr.map(binary);
-console.log(output);
+console.log(output);*/
+
+//2nd method
+/*let arr=[4,8,6,7,2];
+const output=arr.map((x)=>{
+    return x.toString(2); 
+})
+console.log(output);*/
+
+//3rd method
+/*let arr=[4,8,6,7,2];
+const output=arr.map((x)=>x.toString(2))
+console.log(output);*/
+
+
+
+
+
+
+//filter function
+/*let arr=[4,8,6,7,2];
+function isEven(x) {
+    return x%2===0;
+    
+}
+const output=arr.filter(isEven);
+console.log(output);*/
+
+//2nd method
+/*let arr=[4,8,6,7,2];
+const output=arr.filter((x)=>{
+    return x%2===0;
+})
+console.log(output);*/
+
+//3rd method
+/*let arr=[4,8,6,7,2];
+const output=arr.filter((x)=>x%2==0)
+console.log(output);*/
+
+
+
+
+
+
+
+//reduce method
+/*let arr=[4,8,6,7,2];
+function findSum(arr) {
+    let sum=0;
+    for (let index = 0; index < arr.length; index++) {
+         sum += arr[index];
+        
+    }
+    return sum;
+    
+}
+console.log(findSum(arr));*/
+
+//2nd
+/*let arr=[4,8,6,7,2];
+const output=arr.reduce(function(acc, curr) {
+    acc +=curr;
+    return acc;
+    
+}) 
+console.log(output);*/
+
+
+
+//3rd
+/*let arr=[4,8,6,7,2];
+const output=arr.reduce((acc, curr)=> {
+    acc +=curr;
+    return acc;
+    
+}) 
+console.log(output);*/
+
+/*let arr=[4,8,6,7,2];
+function findMax(arr) {
+    let max=0;
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i]>max){
+            max=arr[i];
+        }   
+    }
+    return max;
+    
+}
+console.log(findMax(arr));*/
+    
+    
+
+
